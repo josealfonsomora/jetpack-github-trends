da10c72b50c6ef885466f4a1d268688a6dcf0b6e
Jose Alfonso Mora <josealfonsomora@gmail.com>
2020-09-12 20:33:13 +0200

    Add Navigation component
    We will create the app as a single activity using jetpack navigation component
    [Navigation](https://developer.android.com/guide/navigation)

---
+++ app/src/main/kotlin/com/josealfonsomora/jetpackgithubtrends/ui/NavHostActivity.kt
@@ -0,0 +1,6 @@
+package com.josealfonsomora.jetpackgithubtrends.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import com.josealfonsomora.jetpackgithubtrends.R
+
+class NavHostActivity : AppCompatActivity(R.layout.nav_host_activity)
---
+++ app/src/main/kotlin/com/josealfonsomora/jetpackgithubtrends/ui/reposlist/GithubReposListFragment.kt
@@ -0,0 +1,7 @@
+package com.josealfonsomora.jetpackgithubtrends.ui.reposlist
+
+import androidx.fragment.app.Fragment
+import com.josealfonsomora.jetpackgithubtrends.R
+
+class GithubReposListFragment: Fragment(R.layout.github_repos_list_fragment) {
+}

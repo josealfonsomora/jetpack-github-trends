1bcf59ccba8a475c8be9def482eb4403e1d93f73
Jose Alfonso Mora <josealfonsomora@gmail.com>
2020-09-14 23:08:51 +0200

    Add Timber for logging
    https://github.com/JakeWharton/timber

--- app/src/main/kotlin/com/josealfonsomora/jetpackgithubtrends/GithubTrendsApplication.kt
+++ app/src/main/kotlin/com/josealfonsomora/jetpackgithubtrends/GithubTrendsApplication.kt
@@ -3,6 +3,8 @@ package com.josealfonsomora.jetpackgithubtrends
 import android.app.Application
 import com.facebook.stetho.Stetho
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+import timber.log.Timber.DebugTree
 
 @HiltAndroidApp
 class GithubTrendsApplication : Application() {
@@ -10,6 +12,7 @@ class GithubTrendsApplication : Application() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
             Stetho.initializeWithDefaults(this)
+            Timber.plant(DebugTree())
         }
     }
 }
